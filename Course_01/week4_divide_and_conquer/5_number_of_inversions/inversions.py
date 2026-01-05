2f549b52acb37e5c0d12bf31b8789080e1a9b957
ialghobashy <ialghobashy@gmail.com>
2026-01-05 18:45:39 +0200

    count inversions

--- Course_01/week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ Course_01/week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -1,16 +1,48 @@
 from itertools import combinations
 
 
-def inversions_naive(a):
-    number_of_inversions = 0
-    for i, j in combinations(range(len(a)), 2):
-        if a[i] > a[j]:
-            number_of_inversions += 1
-    return number_of_inversions
+def merge_and_count(a, b):
+    i = 0  # Pointer for left array
+    j = 0  # Pointer for right array
+    merged = []
+    inversions = 0
+    
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            merged.append(a[i])
+            i += 1
+        else:
+            # a[i] > b[j], so b[j] is an inversion with a[i] 
+            # and ALL elements after a[i] in the left array
+            merged.append(b[j])
+            inversions += (len(a) - i)
+            j += 1
+            
+    # Append remaining elements
+    merged.extend(a[i:])
+    merged.extend(b[j:])
+    return merged, inversions
+
+def count_inversions(a):
+    def count_inversions_inner(a):
+        # Base case: a single element has 0 inversions
+        if len(a) <= 1:
+            return a, 0
+        
+        mid = len(a) // 2
+        left_sorted, left_inv = count_inversions_inner(a[:mid])
+        right_sorted, right_inv = count_inversions_inner(a[mid:])
+        
+        # Merge halves and count "split" inversions
+        merged_sorted, split_inv = merge_and_count(left_sorted, right_sorted)
+        
+        return merged_sorted, left_inv + right_inv + split_inv
+    _, inversions = count_inversions_inner(a)
+    return inversions
 
 
 if __name__ == '__main__':
     input_n = int(input())
     elements = list(map(int, input().split()))
     assert len(elements) == input_n
-    print(inversions_naive(elements))
+    print(count_inversions(elements))
